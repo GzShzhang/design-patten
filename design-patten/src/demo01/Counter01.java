77bb377b3425796b56c0385591f8e9c9df227f91
gzshzhang <gzshzhang@qq.com>
2018-08-11 17:07:54 +0800

    edit className

--- design-patten/src/demo01/Counter01.java
+++ design-patten/src/demo01/Counter01.java
@@ -3,7 +3,7 @@ package demo01;
 import java.math.BigDecimal;
 import java.util.Scanner;
 
-public class Counter {
+public class Counter01 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("请输入数字a：");
