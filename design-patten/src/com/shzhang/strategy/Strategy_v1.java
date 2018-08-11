fcddc9d737f5bf306027515db48c7959884b0003
gzshzhang <gzshzhang@qq.com>
2018-08-11 23:02:44 +0800

    商场促销-策略模式

---
+++ design-patten/src/com/shzhang/strategy/Strategy_v1.java
@@ -0,0 +1,34 @@
+package com.shzhang.strategy;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Strategy_v1 {
+	public static void main(String[] args) {
+		//总金额
+		BigDecimal total = new BigDecimal(0);
+		total = amountOfCalculation(total);
+		System.out.println("总计：" + total);
+	}
+	
+	//计算金额
+	private static BigDecimal amountOfCalculation(BigDecimal total) {
+		try {
+			//获取控制台输入内容
+			Scanner scan = new Scanner(System.in);
+			System.out.println("请输入单价：");
+			//单价
+			BigDecimal unitPrice = scan.nextBigDecimal();
+			
+			System.out.println("请输入数量");
+			//数量
+			BigDecimal number = scan.nextBigDecimal();
+			
+			total = unitPrice.multiply(number);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return total;
+	}
+	
+}
